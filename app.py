648b0b5944d23891db9e2bfe64200f70754f203d
Prabal Deb <prdeb@microsoft.com>
2020-04-07 20:34:29 +0530

    Enabling Heroku deployment

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ from covit19.covid19indiaorg import Covid19indiaorg
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 def get_data():
     '''TODO'''
 
